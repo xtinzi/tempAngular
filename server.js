722201125b89b9bd6000c9472815e80ede38e2a5
xtinzi <xtinzi@gmail.com>
2020-05-18 19:26:58 +0200

    redone new

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ var port =  3000;
 var app = express();
 app.use(cors())
 
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
